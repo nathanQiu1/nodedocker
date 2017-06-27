b628ff3fd4508e344f3e6ef3ad710cea2f92eed8
nathanq <nathan_qiu@jabil.com>
2017-06-27 16:05:13 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+﻿//express_demo.js 文件
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/view/" + "test1.html");
+})
+app.get('/test1', function (req, res) {
+   
+    res.sendFile(__dirname + "/view/" + "test1.html");
+   
+})
+app.get('/test2', function (req, res) {
+   
+    res.sendFile(__dirname + "/view/" + "test2.html");
+
+})
+app.get('/test3', function (req, res) {
+   
+    res.sendFile(__dirname + "/view/" + "test3.html");
+
+})
+app.get('/test4', function (req, res) {
+    
+    res.sendFile(__dirname + "/view/" + "test4.html");
+
+})
+app.get('/test5', function (req, res) {
+   
+    res.sendFile(__dirname + "/view/" + "test5.html");
+
+})
+
+
+app.use(express.static('img'));
+var server = app.listen(8081, function () {
+
+  
+    var port = server.address().port
+
+    console.log("Service has started, port:"+ port)
+
+})
